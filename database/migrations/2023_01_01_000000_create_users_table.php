2742ec1ab7f167b6dad91715ceca15c8fd03bb07
sim1clstl <simone.celestial@student.ateneo.edu>
2025-09-29 23:28:22 +0800

    Update 2023_01_01_000000_create_users_table.php

--- database/migrations/2023_01_01_000000_create_users_table.php
+++ database/migrations/2023_01_01_000000_create_users_table.php
@@ -12,6 +12,7 @@ return new class extends Migration {
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            
         });
     }
     public function down() {
