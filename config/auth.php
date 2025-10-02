a816462e4e5870a8e9cd32d56bded5f8fd3dd69a
sim1clstl <simone.celestial@student.ateneo.edu>
2025-10-02 23:17:12 +0800

    Update auth.php

--- config/auth.php
+++ config/auth.php
@@ -17,7 +17,7 @@ return [
         'guard' => env('AUTH_GUARD', 'web'),
         'passwords' => env('AUTH_PASSWORD_BROKER', 'users'),
     ],
-
+ 
     /*
     |--------------------------------------------------------------------------
     | Authentication Guards
